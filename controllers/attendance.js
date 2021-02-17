f0844a4524a1281c5c634da6caaa414ced053764
aijalonti <aijalon.ti@gmail.com>
2021-02-17 11:45:58 -0300

    feat: Persistence DataBase

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,10 +1,14 @@
+const Attendance = require('../models/attendanceModel')
+
 module.exports = (app) => {
   app.get("/atendimentos", (req, res) =>
     res.send("Você está na rota de atendimentos e está realizando um GET")
   );
 
   app.post("/atendimentos", (req, res) => {
-    console.log(req.body);
+    const attendance = req.body
+    
+    Attendance.create(attendance)
     res.send("Você está na rota de atendimentos e está realizando um POST");
   });
 };
